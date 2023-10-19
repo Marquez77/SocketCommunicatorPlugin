dc59eada9a256105cb6e33d03b359ff8b2691930
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-10-20 06:23:18 +0900

    Release 1.0.0

--- src/main/java/me/marquez/socketplugin/SocketCommunicatorPlugin.java
+++ src/main/java/me/marquez/socketplugin/SocketCommunicatorPlugin.java
@@ -1,4 +1,14 @@
 package me.marquez.socketplugin;
 
-public class SocketCommunicatorPlugin {
+import me.marquez.socket.SocketManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SocketCommunicatorPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        SocketManager.initialize();
+        getLogger().info("socket-communicator-api has been initialized!");
+    }
+
 }
